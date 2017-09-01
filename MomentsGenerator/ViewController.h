69344ab545166a1e2d43a98a11099b234899666c
zhangchi <zhangchi@syswin.com>
2017-09-01 10:59:00 +0800

    Initial Commit

---
+++ MomentsGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MomentsGenerator
+//
+//  Created by Zark on 2017/9/1.
+//  Copyright © 2017年 Zarky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
